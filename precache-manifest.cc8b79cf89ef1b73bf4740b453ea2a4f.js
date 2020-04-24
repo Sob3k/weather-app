e7e550f7766cce1a5e83ab23db65098587290ddc
Sob3k <sebastian977@poczta.fm>
2020-04-24 18:04:06 +0200

    Updates

---
+++ precache-manifest.cc8b79cf89ef1b73bf4740b453ea2a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a314b6bf7479b42113d9a309502aa4",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f7706dfcb730d887d0ba",
+    "url": "/weather-app/static/css/main.e80c1cb9.chunk.css"
+  },
+  {
+    "revision": "63feb66299392057e671",
+    "url": "/weather-app/static/js/2.eaaff0db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.eaaff0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7706dfcb730d887d0ba",
+    "url": "/weather-app/static/js/main.4303becb.chunk.js"
+  },
+  {
+    "revision": "8ad2e2c3d0b0209b3902",
+    "url": "/weather-app/static/js/runtime-main.4479c0a4.js"
+  }
+]);
